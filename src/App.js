ee13d243d27f6446fb5bfebc81ec57458eec0d76
cjsauer18 <57496346+cjsauer18@users.noreply.github.com>
2020-02-12 22:47:54 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Notes from "./notes";
+import Note from "./note";
+function App() {
+  return (
+    <div>
+      <Notes />
+    </div>
+  );
+}
+
+export default App;
