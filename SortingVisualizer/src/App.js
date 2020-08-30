7b4542ac129bfd105fa8a45eea63531cb166cec2
Chris Sauer <57496346+cjsauer18@users.noreply.github.com>
2020-08-30 11:31:23 -0400

    Add files via upload

---
+++ SortingVisualizer/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import SortingVisualizer from "./node/SortingVisualizer.jsx";
+import Toolbar from "./node/Toolbar.jsx";
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualizer> </SortingVisualizer>
+    </div>
+  );
+}
+
+export default App;
